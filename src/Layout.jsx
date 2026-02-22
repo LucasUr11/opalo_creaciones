a6017a4728a7b001a2ca85eb5d50bfb31109f129
Lucas Urquiza <lucasestudios2021@gmail.com>
2026-02-22 20:15:47 -0300

    Opalo creaciones

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CartProvider } from '@/components/store/CartContext';
-import Navbar from '@/components/store/Navbar';
+import Navbar from '@/components/store/NavBar.jsx';
 
 export default function Layout({ children }) {
   return (
